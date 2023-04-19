50d0d7d1ada603b6b370e0af27c10165054500c8
Anderson Motta <71981279+1andersonmotta@users.noreply.github.com>
2023-04-19 12:55:43 -0300

    Add files via upload
    instale node modules com npm install

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import express, {request, response} from 'express';
+import { activityCheckin, 
+         activityCheckout, 
+         listActivities, 
+         removeActivity } from './controllers/activitesController.js';
+import { insertVehicles, 
+        listVehicles, 
+        removeVehicle, 
+        updateVehicles } from './controllers/vehiclesController.js';
+
+const app = express();
+
+app.use((req, res, next) => {
+
+    res.header("Access-Control-Allow-Origin", "*");// dentro do '*' poderia ser qual site poderia fazer a requisiçao.
+
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+
+    next();
+
+})
+
+
+app.use(express.json());
+
+app.get('/api/ping', (request, response) => {
+    response.send({
+        message: 'pong'
+    })
+});
+
+/* Endpoints vehicles*/
+app.get('/api/vehicles', listVehicles);
+
+app.post('/api/vehicles', insertVehicles);
+
+app.put('/api/vehicles/:id', updateVehicles);
+
+app.delete('/api/vehicles/:id', removeVehicle);
+
+/* Endpoints Activites */
+
+app.post('/api/activities/checkin', activityCheckin);
+
+app.put('/api/activities/checkout', activityCheckout);
+
+app.delete('/api/activities/:id', removeActivity);
+
+app.get('/api/activities', listActivities);
+
+app.listen(8000, () => {
+    console.log("Servidor rodando na porta 8000...");
+});
